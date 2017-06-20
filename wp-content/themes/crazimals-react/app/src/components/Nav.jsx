f4e683be7b5bd1bb4dcf00feb55333a12a920183
Travis Smith <travis.smith@weather.com>
2017-06-20 14:38:20 -0400

    Fix home link

--- wp-content/themes/crazimals-react/app/src/components/Nav.jsx
+++ wp-content/themes/crazimals-react/app/src/components/Nav.jsx
@@ -19,8 +19,8 @@ export default class Nav extends React.Component {
         <nav className="navbar w-nav" style={navStyle} data-animation="default" data-collapse="none"
              data-duration="400">
           <div className="container-2 w-container">
-            <a className="brand w-nav-brand" href="http://crazimals.com">
-              <img className="image-3 img" src="/wp-content/themes/crazimals/app/images/craz_logo_white.svg"
+            <a className="brand w-nav-brand" href="/">
+              <img className="image-3 img" src="/wp-content/themes/crazimals-react/app/images/craz_logo_white.svg"
                    width="222.5"/>
             </a>
             <div className="w-nav-button">
